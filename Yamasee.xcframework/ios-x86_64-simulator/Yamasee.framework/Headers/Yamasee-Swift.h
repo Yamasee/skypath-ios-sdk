1bc8346c981e4f7c3c2dffa37c2ab476875a990a
Alex Kovalov <alex.kovalov@requestum.com>
2020-08-17 16:03:19 +0300

    v1.6.0

--- Yamasee.xcframework/ios-x86_64-simulator/Yamasee.framework/Headers/Yamasee-Swift.h
+++ Yamasee.xcframework/ios-x86_64-simulator/Yamasee.framework/Headers/Yamasee-Swift.h
@@ -186,6 +186,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
+@import Foundation;
 @import MapKit;
 @import UIKit;
 #endif
@@ -204,6 +205,14 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 # pragma clang attribute push(__attribute__((external_source_symbol(language="Swift", defined_in="Yamasee",generated_declaration))), apply_to=any(function,enum,objc_interface,objc_category,objc_protocol))
 # pragma pop_macro("any")
 #endif
+
+
+
+
+
+
+
+
 
 
 
