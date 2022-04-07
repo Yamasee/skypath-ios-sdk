f319f76375ad9371d7eef474db9795d47029bd58
Alex Kovalov <alex.kovalov@requestum.com>
2022-04-07 16:00:59 +0300

    Update to v2.0-beta2

---
+++ SkyPathSDK.xcframework/ios-arm64/SkyPathSDK.framework/Headers/SkyPathSDK.h
@@ -0,0 +1,14 @@
+//
+//  SkyPathSDK.h
+//  SkyPathSDK
+//
+//  Created by Alex Kovalov on 23.03.2022.
+//  Copyright © 2022 Yamasee. All rights reserved.
+//
+
+#ifndef SkyPathSDK_h
+#define SkyPathSDK_h
+
+#import <SkyPathSDK/h3api.h>
+
+#endif /* SkyPathSDK_h */
