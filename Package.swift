4882c4a1fcf27e93367ef3bc64882c28656b0d04
Alex Kovalov <alex@skypath.io>
2024-08-01 12:15:05 +0300

    Update to 2.2.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.10
 import PackageDescription
 
 let package = Package(
@@ -10,18 +10,18 @@ let package = Package(
         .library(name: "SkyPathSDK", targets: ["SkyPathSDKTarget"])
     ],
     dependencies: [
-        .package(url: "https://github.com/GEOSwift/geos.git", exact: "8.1.0")
+        .package(url: "https://github.com/Yamasee/GEOSwift", from: "10.2.0")
     ],
     targets: [
         .binaryTarget(
             name: "SkyPathSDK",
-            url: "https://github.com/Yamasee/skypath-ios-sdk/releases/download/v2.2.1/SkyPathSDK.xcframework.zip",
-            checksum: "3900152b3b05f9c1783197fe0533fc0db8a60b1de6da2f3904791109e53f0417"),
+            url: "https://github.com/Yamasee/skypath-ios-sdk/releases/download/v2.2.2/SkyPathSDK.xcframework.zip",
+            checksum: "4500bcf9e9b3a6930f48144e6e5e863d995642c82d91e463f726a4bf46417c6c"),
         .target(
             name: "SkyPathSDKTarget",
             dependencies: [
                 .target(name: "SkyPathSDK"),
-                .product(name: "geos", package: "geos")
+                .product(name: "GEOSwift", package: "GEOSwift")
             ])
     ]
 )
--- SkyPath.podspec
+++ SkyPath.podspec
@@ -1,10 +1,10 @@
 Pod::Spec.new do |s|
     
-    sdk_version = '2.2.1'
+    sdk_version = '2.2.2'
 
     s.name = 'SkyPath'
     s.version = sdk_version
-    s.summary = 'The world’s leading turbulence and auto-PIREPs data source.'
+    s.summary = 'SkyPath, Your Ride Quality Partner'
     s.homepage = 'https://github.com/Yamasee/skypath-ios-sdk'
     s.author = { 'SkyPath' => 'info@skypath.io' }
     s.license = { :type => 'Commercial' }
@@ -18,13 +18,13 @@ Pod::Spec.new do |s|
         SkyPath iOS SDK must be used according to the SkyPath Terms of Use https://skypath.io/terms/.
         LICENSE
     }
-    s.documentation_url = 'https://yamasee.github.io/skypath-ios-sdk-docs'
+    s.documentation_url = 'https://docs.skypath.io'
 
     s.platform = :ios
-    s.source = { :http => "#{s.homepage}/releases/download/v#{sdk_version}/SkyPathSDK.xcframework.zip" } 
+    s.source = { :http => "#{s.homepage}/releases/download/v#{sdk_version}/SkyPathSDK.xcframework.zip" }
     s.ios.deployment_target = '14.0'
     s.swift_version = '5.10'
     s.ios.vendored_frameworks = 'SkyPathSDK.xcframework'
-    s.dependency 'geos', '~> 8.1'
+    s.dependency 'GEOSwift', '~> 10.2.0'
 
 end 
