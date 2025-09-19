9a066dae256fab951abb9d4a431c994b61b4ef63
Alex Kovalov <alex@skypath.io>
2025-09-19 14:42:43 +0300

    Update to 3.0.6

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SkyPathSDK",
-            url: "https://github.com/Yamasee/skypath-ios-sdk/releases/download/v3.0.5/SkyPathSDK.xcframework.zip",
-            checksum: "043f7b0226eeb8d1ff9ce23e036c616f61456d4c549e86a1cd0e72b8d536d400"),
+            url: "https://github.com/Yamasee/skypath-ios-sdk/releases/download/v3.0.6/SkyPathSDK.xcframework.zip",
+            checksum: "285a091c334257e86287c02e946d66a035be6483ff85a05f567c79697e9dffbe"),
         .target(
             name: "SkyPathSDKTarget",
             dependencies: [
