2b343d4b23a99eaf6f35c45b0146398bc0a50ab9
Alex Kovalov <alex@skypath.io>
2025-10-15 11:05:14 +0300

    Update to 3.0.10

--- Package.swift
+++ Package.swift
@@ -10,20 +10,21 @@ let package = Package(
         .library(name: "SkyPathSDK", targets: ["SkyPathSDKTarget"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Yamasee/GEOSwift.git", from: "11.1.0"),
-        .package(url: "https://github.com/mapbox/turf-swift", from: "4.0.0")
+        .package(url: "https://github.com/mapbox/turf-swift", from: "4.0.0"),
+        .package(url: "https://github.com/GEOSwift/geos", from: "9.0.0")
     ],
     targets: [
         .binaryTarget(
             name: "SkyPathSDK",
-            url: "https://github.com/Yamasee/skypath-ios-sdk/releases/download/v3.0.9/SkyPathSDK.xcframework.zip",
-            checksum: "83521f4b1ed3e42470ffaa4950fd5957f5478dde2d36a55fbf02889eca66a9e3"),
+            url: "https://github.com/Yamasee/skypath-ios-sdk/releases/download/v3.0.10/SkyPathSDK.xcframework.zip",
+            checksum: "b5bd8eab6bb080f8cf7df372c3a9d6cb6fb79c0d39e5ee83ce026725bf6c6259"),
         .target(
             name: "SkyPathSDKTarget",
             dependencies: [
                 .target(name: "SkyPathSDK"),
-                .product(name: "GEOSwift", package: "GEOSwift"),
-                .product(name: "Turf", package: "turf-swift")
+                .product(name: "Turf", package: "turf-swift"),
+                .product(name: "geos", package: "geos")
+
             ])
     ]
 )
