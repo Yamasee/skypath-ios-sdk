d9caeeb9395e88b60f7c3f1c6aeb67e415cbab86
Alex Kovalov <alex@skypath.io>
2026-05-27 19:08:25 +0300

    Update to 3.1.1

--- Package.swift
+++ Package.swift
@@ -11,20 +11,21 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/mapbox/turf-swift", from: "4.0.0"),
-        .package(url: "https://github.com/GEOSwift/geos", from: "9.0.0")
+        .package(url: "https://github.com/GEOSwift/geos", from: "9.0.0"),
+        .package(url: "https://github.com/Yamasee/swift-protobuf", from: "1.32.0")
     ],
     targets: [
         .binaryTarget(
             name: "SkyPathSDK",
-            url: "https://github.com/Yamasee/skypath-ios-sdk/releases/download/v3.0.19/SkyPathSDK.xcframework.zip",
-            checksum: "b51e1b2686a3bccac5b1f055b777d814443a19595aae9b8b244c493ec7d6c99a"),
+            url: "https://github.com/Yamasee/skypath-ios-sdk/releases/download/v3.1.1/SkyPathSDK.xcframework.zip",
+            checksum: "e301eee35abc48214df9ae2b2c15fbae9266b85b82a9aaa2d2909820224ea47d"),
         .target(
             name: "SkyPathSDKTarget",
             dependencies: [
                 .target(name: "SkyPathSDK"),
                 .product(name: "Turf", package: "turf-swift"),
-                .product(name: "geos", package: "geos")
-
+                .product(name: "geos", package: "geos"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf")
             ])
     ]
 )
