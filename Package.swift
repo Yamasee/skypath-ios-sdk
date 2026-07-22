ef5163ab7e5c1a97297aea66c1652d7036887a35
Alex Kovalov <alex@skypath.io>
2026-07-22 16:08:43 +0300

    Update to 3.1.3

--- Package.swift
+++ Package.swift
@@ -11,21 +11,21 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/mapbox/turf-swift", from: "4.0.0"),
-        .package(url: "https://github.com/GEOSwift/geos", from: "9.0.0"),
-        .package(url: "https://github.com/Yamasee/swift-protobuf", from: "1.32.0")
+        .package(url: "https://github.com/GEOSwift/geos", from: "9.0.0")
     ],
     targets: [
         .binaryTarget(
             name: "SkyPathSDK",
-            url: "https://github.com/Yamasee/skypath-ios-sdk/releases/download/v3.1.2/SkyPathSDK.xcframework.zip",
-            checksum: "35cdf71593a940f898006e35bf6137cd5ec2acd8f9673c8beed073f5e097ff4b"),
+            url: "https://github.com/Yamasee/skypath-ios-sdk/releases/download/v3.1.3/SkyPathSDK.xcframework.zip",
+            checksum: "85e9e06ffe4a0cdc1035ea0f39ca8fbee9b3e02f8e0b8bc77763e5c75dabb0c2"),
         .target(
             name: "SkyPathSDKTarget",
             dependencies: [
                 .target(name: "SkyPathSDK"),
                 .product(name: "Turf", package: "turf-swift"),
-                .product(name: "geos", package: "geos"),
-                .product(name: "SwiftProtobuf", package: "swift-protobuf")
-            ])
+                .product(name: "geos", package: "geos")
+            ],
+            path: "Sources/SkyPathSDKTarget"
+        )
     ]
 )
--- Sources/SkyPathSDKTarget/Sources.swift
+++ Sources/SkyPathSDKTarget/Sources.swift
@@ -1,1 +1,2 @@
-
+// This file is required by SPM to serve as a wrapper target for binary distribution.
+import Foundation
